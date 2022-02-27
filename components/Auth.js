28ea92c1f3afdc80ef6b569293871173286349bc
hirano <rhi691508@gmail.com>
2022-02-27 22:56:36 +0900

    ユーザーがログインする際を条件分岐で処理

--- components/Auth.js
+++ components/Auth.js
@@ -43,6 +43,30 @@ export default function Auth() {
     }
   };
 
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <div className="max-w-md w-full space-y-8">
       <div>
