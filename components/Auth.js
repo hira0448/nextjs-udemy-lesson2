9f88d930fa565342f5cafead27976de664b92e24
hirano <rhi691508@gmail.com>
2022-03-02 00:04:56 +0900

    usernameとpasswordフォームの編集

--- components/Auth.js
+++ components/Auth.js
@@ -84,24 +84,30 @@ export default function Auth() {
         <div className="rounded-md shadow-sm -space-y-px">
           <div>
             <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
+              name="username"
+              type="text"
+              autoComplete="username"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
           <div>
             <input
-              id="password"
               name="password"
               type="password"
               autoComplete="current-password"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
         </div>
