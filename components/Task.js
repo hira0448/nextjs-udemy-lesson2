4b2621f573a725136d43327c5276f0d9c3d7635d
hirano <rhi691508@gmail.com>
2022-03-14 23:45:09 +0900

    spanタグでtaskのtitleを表示させる処理

--- components/Task.js
+++ components/Task.js
@@ -3,6 +3,9 @@ export default function Task({ task }) {
     <div>
       <span>{task.id}</span>
       {" : "}
+      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+        {task.title}
+      </span>
     </div>
   );
 }
