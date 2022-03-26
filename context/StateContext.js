51292b44635cf7a5d6e0f8d991879c9298c40e5b
hirano <rhi691508@gmail.com>
2022-03-26 16:29:16 +0900

    id: 0, title: ""の初期値の関数を定義

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,3 +1,18 @@
 import { createContext, useState } from "react/cjs/react.production.min";
 
 export const StateContext = createContext();
+
+export default function StateContextProvider(props) {
+  const [selectedTask, setSelectedTask] = useState({ id: 0, title: "" });
+
+  return (
+    <StateContext.Provider
+      value={{
+        selectedTask,
+        setSelectedTask,
+      }}
+    >
+      {props.children}
+    </StateContext.Provider>
+  );
+}
