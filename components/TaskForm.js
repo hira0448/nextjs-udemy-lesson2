24608b126c83de4bbea67d8d6c2ccaba5ec7ddea
hirano <rhi691508@gmail.com>
2022-03-27 06:12:40 +0900

    TaskFormへCookieを追加

---
+++ components/TaskForm.js
@@ -0,0 +1,5 @@
+import { useContext } from "react/cjs/react.production.min";
+import { StateContext } from "../context/StateContext";
+import Cookies from "universal-cookie";
+
+const cookie = new Cookies();
