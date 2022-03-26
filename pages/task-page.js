cc00bccbf5f7ac8e3c945e592d309165423d102a
hirano <rhi691508@gmail.com>
2022-03-26 16:32:25 +0900

    task-pageにStateContextProviderをインポートし、レイアウトをラップ

--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,7 @@ import Task from "../components/Task";
 import { getAllTasksData } from "../lib/tasks";
 import useSWR from "swr";
 import { useEffect } from "react/cjs/react.production.min";
+import StateContextProvider from "../context/StateContext";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task`;
@@ -20,31 +21,33 @@ export default function TaskPage({ staticfilterdTasks }) {
   }, []);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDelete={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page" passHref>
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            className="w-6 h-6 mr-3"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDelete={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page" passHref>
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              className="w-6 h-6 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
