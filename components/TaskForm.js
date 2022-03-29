7b8e6af96382d9c50d3392eb1351f3c87d841815
hirano <rhi691508@gmail.com>
2022-03-29 21:35:34 +0900

    createとupdate関数をTaskForm関数に格納

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -6,45 +6,44 @@ const cookie = new Cookies();
 
 export default function TaskForm({ taskCreated }) {
   const { SelectedTask, setSelectedTask } = useContext(StateContext);
-}
-
-const create = async (e) => {
-  e.preventDefault();
-  await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks`, {
-    method: "POST",
-    body: JSON.stringify({ title: SelectedTask.title }),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `JWT ${cookie.get("access_token")}`,
-    },
-  }).then((res) => {
-    if (res.status === 401) {
-      alert("JWT Taken not valid");
-    }
-  });
-  setSelectedTask({ id: 0, title: "" });
-  taskCreated();
-};
-
-const update = async (e) => {
-  e.preventDefault();
-  await fetch(
-    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${selectedTask.id}/`,
-    {
-      method: "PUT",
-      body: JSON.stringify({
-        title: selectedTask.title,
-      }),
+  const create = async (e) => {
+    e.preventDefault();
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks`, {
+      method: "POST",
+      body: JSON.stringify({ title: SelectedTask.title }),
       headers: {
-        "Context-Type": "application/json",
+        "Content-Type": "application/json",
         Authorization: `JWT ${cookie.get("access_token")}`,
       },
-    }
-  ).then((res) => {
-    if (res.status === 401) {
-      alert("JWT Taken not valid");
-    }
-  });
-  setSelectedTask({ id: 0, title: "" });
-  taskCreated();
-};
+    }).then((res) => {
+      if (res.status === 401) {
+        alert("JWT Taken not valid");
+      }
+    });
+    setSelectedTask({ id: 0, title: "" });
+    taskCreated();
+  };
+
+  const update = async (e) => {
+    e.preventDefault();
+    await fetch(
+      `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${selectedTask.id}/`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          title: selectedTask.title,
+        }),
+        headers: {
+          "Context-Type": "application/json",
+          Authorization: `JWT ${cookie.get("access_token")}`,
+        },
+      }
+    ).then((res) => {
+      if (res.status === 401) {
+        alert("JWT Taken not valid");
+      }
+    });
+    setSelectedTask({ id: 0, title: "" });
+    taskCreated();
+  };
+}
